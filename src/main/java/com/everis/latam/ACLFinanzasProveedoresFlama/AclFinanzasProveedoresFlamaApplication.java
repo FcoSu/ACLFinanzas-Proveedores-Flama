74c5cabd46c9a1ede5b836c8faf36515d85ad747
Francisco <panchoseur@gmail.com>
2021-03-30 11:43:18 -0300

    first commit

---
+++ src/main/java/com/everis/latam/ACLFinanzasProveedoresFlama/AclFinanzasProveedoresFlamaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.latam.ACLFinanzasProveedoresFlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclFinanzasProveedoresFlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclFinanzasProveedoresFlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/latam/ACLFinanzasProveedoresFlama/AclFinanzasProveedoresFlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.latam.ACLFinanzasProveedoresFlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclFinanzasProveedoresFlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
